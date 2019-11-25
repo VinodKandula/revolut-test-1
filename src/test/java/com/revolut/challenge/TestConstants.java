ac18d64248270579d2fb705d403a3e9cfed98c98
Egor Aralov <egor.aralov@quandoo.com>
2019-11-25 23:52:10 +0100

    chore(tests): do basic test setup

---
+++ src/test/java/com/revolut/challenge/TestConstants.java
@@ -0,0 +1,9 @@
+package com.revolut.challenge;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class TestConstants {
+    public static final String INTEGRATION_TAG = "integration";
+}
